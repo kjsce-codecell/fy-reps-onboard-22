a4b5fd1edb0b6c751096df4b68971fad7cf6c089
Vatsal <bevatsal1122@gmail.com>
2023-01-04 02:36:02 +0530

    feat: adds right container rendering for step 1 and step 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
+import detailsStyles from "../styles/Details.module.css";
 import { useState } from "react";
 import { Steps, ConfigProvider } from "antd";
 const Step = Steps.Step;
@@ -17,6 +18,14 @@ export default function Home() {
 		{
 			id: 2,
 			title: "Step 2",
+		},
+		{
+			id: 3,
+			title: "Step 3",
+		},
+		{
+			id: 4,
+			title: "Step 3",
 		},
 		{
 			id: 3,
@@ -40,7 +49,7 @@ export default function Home() {
 						<ConfigProvider
 							theme={{
 								token: {
-                  colorText:"#fff",
+                  				colorText:"#fff",
 									colorPrimaryActive: "#F5222D",
 									colorPrimaryTextActive: "#F5222D",
                   
@@ -64,7 +73,78 @@ export default function Home() {
 							</Steps>
 						</ConfigProvider>
 					</div>
-					<div className={styles.rightcontainer}></div>
+					<div className={styles.rightcontainer}>
+						{currentSlide === 0 && (
+							<div className={detailsStyles.oneSection}>
+								<div className={detailsStyles.sectionHeader}>
+									<span>Step {currentSlide+1}/5</span>
+									<h1>Lets start with your Personal Details</h1>
+									<p>Please fill in your Name, Branch & Email ID</p>
+								</div>
+								<div className={detailsStyles.sectionContent}>
+									<div className={detailsStyles.oneField}>
+										<label>Enter Your Email ID</label>
+										<input type="text" />
+									</div>
+									<div className={detailsStyles.oneField}>
+										<label>Enter Your Name</label>
+										<input type="text" />
+									</div>
+									<div className={detailsStyles.oneField}>
+										<label>Enter Your Branch</label>
+										<input type="text" />
+									</div>
+									<div>
+										<button type="button">Submit</button>
+									</div>
+								</div>
+							</div>
+						)}
+
+						{currentSlide === 1 && (
+							<div className={detailsStyles.oneSection}>
+							<div className={detailsStyles.sectionHeader}>
+								<span>Step {currentSlide+1}/5</span>
+								<h1>Showcase your Profiles</h1>
+								<p>Show us What you got</p>
+							</div>
+							<div className={detailsStyles.sectionContent}>
+								<div className={detailsStyles.oneField}>
+									<label>GitHub Profile</label>
+									<input type="text" />
+								</div>
+								<div className={detailsStyles.oneField}>
+									<label>CP Platform Profile</label>
+									<input type="text" />
+								</div>
+								<div className={detailsStyles.oneField}>
+									<label>LinkedIn Profile</label>
+									<input type="text" />
+								</div>
+								{/* <div className={detailsStyles.oneField}>
+									<label>Describe yourself in one line</label>
+									<input type="text" />
+								</div> */}
+								<div>
+									<button type="button">Submit</button>
+								</div>
+							</div>
+						</div>
+						)}
+
+						{currentSlide === 2 && (
+							<h1>HelloWorld 3</h1>
+						)}
+
+						{currentSlide === 3 && (
+							<h1>HelloWorld 4</h1>
+						)}
+
+						{currentSlide === 4 && (
+							<h1>HelloWorld 5</h1>
+						)}
+						
+					</div>
 				</div>
 			</main>
 		</>
