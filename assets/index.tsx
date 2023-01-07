0972ca545f9bf556b7ab5c5c0476968a1c638425
Vatsal <bevatsal1122@gmail.com>
2023-01-07 17:33:55 +0530

    feat: updates Head in index.tsx

--- assets/index.tsx
+++ assets/index.tsx
@@ -4,6 +4,7 @@ import fire from './fire.svg'
 import legal from './legal.svg'
 import CodecellLogo from './CodecellLogo.png'
 import loader from './loader.gif';
+import codecellFavicon from './codecellFavicon.png';
 
 export {
     user,
@@ -11,5 +12,6 @@ export {
     legal,
     fire,
     CodecellLogo,
-    loader
+    loader,
+    codecellFavicon
 }
